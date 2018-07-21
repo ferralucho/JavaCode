a6de0e0244b0f29137d2c807d47a5761699c7c88
Luciano Ferrari <lucianodarioferrari@gmail.com>
2018-07-21 20:20:17 -0300

    update basics

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -1,3 +1,5 @@
+  //package com.semanticsquare.basics;
+  
   class BasicsDemo {
     // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
     static void print() {
@@ -139,9 +141,121 @@
 		//double d1 = 3.14;
 		//double d2 = 5.15;
 		//System.out.println("d1 | d2: " + (d1 | d2));
-  }
+    }
+  
+    static void switchExample() {
+	    System.out.println("\nInside switchExample ...");
+	    int month = 3;
+	    switch (month) {
+		    case 1: System.out.println("January");
+	                break;
+		    case 2: System.out.println("February");
+	                break;
+	        case 3: System.out.println("March");
+	                break;
+		    default: System.out.println("April");
+	    }
+    }
+  
+    static void labeledBreak() {
+	    System.out.println("\nInside labeledBreak ...");
+	    int num = 0;
+		
+	    outermost: for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j < 10; j++) {    
+	            if (i == 5 && j == 5) {
+	                break outermost;
+	            }
+	            num++;
+	        }
+	    }
+		
+	    System.out.println("num: " + num); // prints 55		
+    }
+	
+	static void labeledContinue() {
+	    System.out.println("\nInside labeledContinue ...");
+	    int num = 0;
+		
+	    outermost: for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j < 10; j++) {    
+	            if (i == 5 && j == 5) {
+	                continue outermost;
+	            }
+	            num++;
+	        }
+	    }
+		
+	    System.out.println("num: " + num); // prints 55		
+    }
+	
+	static void stringExamples() {
+		System.out.println("\nInside stringExamples ...");
+		String s = "hello world!";
+		System.out.println("s: " + s);
+		
+		System.out.println("\ns.length(): " + s.length());
+		System.out.println("s.isEmpty(): " + s.isEmpty());
+		
+		// Comparison
+		System.out.println("\ns.equals(\"HELLO WORLD!\"): " + s.equals("HELLO WORLD!"));
+		System.out.println("s.equalsIgnoreCase(\"HELLO WORLD!\"): " + s.equalsIgnoreCase("HELLO WORLD!"));
+		System.out.println("s.compareTo(\"HELLO WORLD!\"): " + s.compareTo("HELLO WORLD!"));
+		
+		// Searching
+		System.out.println("\ns.contains(\"HELLO WORLD!\"): " + s.contains("HELLO WORLD!"));
+		System.out.println("s.contains(\"world!\"): " + s.contains("world!"));
+		System.out.println("s.startsWith(\"HELLO WORLD!\"): " + s.startsWith("HELLO WORLD!"));
+		System.out.println("s.startsWith(\"hello world!\"): " + s.startsWith("hello world!"));
+		System.out.println("s.endsWith(\"!\"): " + s.endsWith("!"));
+		System.out.println("s.indexOf(\"lo\"): " + s.indexOf("lo"));
+		System.out.println("s.indexOf(\"o\"): " + s.indexOf('o'));
+		System.out.println("s.lastIndexOf(\"o\"): " + s.lastIndexOf('o'));
+		
+		// Examining individual characters
+		System.out.println("\ns.charAt(4): " + s.charAt(4));
+		
+		// Extracting substrings
+		System.out.println("\ns.substring(4): " + s.substring(4));
+		System.out.println("s.substring(4, 9): " + s.substring(4, 9));
+		
+		// Case conversions
+		System.out.println("\ns.toUpperCase(): " + s.toUpperCase());
+		System.out.println("s.toLowerCase(): " + s.toLowerCase());
+		
+		System.out.println("\ns.trim(): " + s.trim()); // returns a copy of string after trimming any leading & trailing white spaces
+		
+		// Replace
+		System.out.println("\ns.replaceAll(\"o\", \"r\"): " + s.replaceAll("o", "r"));
+		
+		// Split
+		System.out.println("\ns.split(\"o\"): ");
+		String[] sa = s.split("o");
+		for (String temp : sa) {
+			System.out.println(temp);
+		}
+		
+		System.out.println("\ns.valueOf(1.3): " + s.valueOf(1.3));
+	}
+	
+	static void stringPool() {
+	  System.out.println("\nInside stringPool ...");
+	  String s1 = "hello!";
+	  String s2 = "hello!";
+	  String s3 = "hello!".intern();
+	  String s4 = new String("hello!");	 
+	  String s5 = "lo!";
+
+      System.out.println("s1 == s2: " + (s1 == s2));
+      System.out.println("s1 == s3: " + (s1 == s3));
+      System.out.println("s1 == s4: " + (s1 == s4));
+      System.out.println("s1 == s4.intern(): " + (s1 == s4.intern()));
+      System.out.println("s1 == \"hel\" + \"lo!\": " + (s1 == "hel" + "lo!"));
+      System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5));
+    }
 		
 	public static void main(String[] args) {	
+	  // Language Basics 1
 	  //print();				
 	  //primitives();
 	  //typeCasting();
@@ -151,6 +265,13 @@
       varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);	  
       varargsOverload(true);*/
 	  //charTypePromotion();
-	  bitwiseOperators();
+	  //bitwiseOperators();
+	  //switchExample();
+	  //labeledBreak();
+	  //labeledContinue();
+      // import demo
+      // package creation demo
+      //stringExamples();	
+      stringPool();	  
     }       
   }
